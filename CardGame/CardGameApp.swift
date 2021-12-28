330fd1dad5c27ae985c675fe7b31471d0a8ad1b8
AJ Green <aj@greenvariable.com>
2021-12-28 16:24:57 -0500

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by AJ Green on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
