3a7ee52a8ff2eec1061437c1fb9ccf553ed2b486
AJ Green <aj@greenvariable.com>
2021-12-28 23:27:01 -0500

    Game Screen setup

--- CardGame/ContentView.swift
+++ CardGame/ContentView.swift
@@ -9,10 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background")
+            VStack{
+                Spacer()
+                Image("logo")
+                
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                
+                Image("dealbutton")
+                
+                HStack{
+                    Spacer()
+                    VStack{
+                    Text("Player")
+                    Text("0")
+                    }
+                    Spacer()
+                    VStack{
+                    Text("CPU")
+                    Text("0")
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
